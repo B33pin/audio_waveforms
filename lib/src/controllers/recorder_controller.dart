9cf5ac3384eec8f542162d8ad66a4d353819d4fa
B33pin <bipin.bhandari1510@gmail.com>
2022-05-20 23:49:26 +0545

    added schedule binder on scrolled position

--- lib/src/controllers/recorder_controller.dart
+++ lib/src/controllers/recorder_controller.dart
@@ -196,7 +196,7 @@ class RecorderController extends ChangeNotifier {
 
   ///sets [shouldClearLabels] flag to false
   void revertClearlabelCall() {
-    WidgetsBinding.instance.addPostFrameCallback((_) {
+    WidgetsBinding.instance?.addPostFrameCallback((_) {
       shouldClearLabels = false;
       notifyListeners();
     });
@@ -255,7 +255,7 @@ class RecorderController extends ChangeNotifier {
   ///[Internally] used to set scrolled position
   ///to duration.
   void setScrolledPostionDuration(int duration) {
-    WidgetsBinding.instance.addPostFrameCallback((_) {
+    WidgetsBinding.instance?.addPostFrameCallback((_) {
       _currentScrolledDuration.value = duration;
       notifyListeners();
     });
